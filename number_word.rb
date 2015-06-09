0276c8b2b217f68b8c0c83c61448dbf9590b10e2
Kumar <akarsh.mit@gmail.com>
2015-06-09 17:49:16 +0530

    Convert 1 digit numbers to words

--- number_word.rb
+++ number_word.rb
@@ -1,2 +1,12 @@
 class NumberWord
+
+  NUMBER_WORD = { 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine',
+                  10 => 'ten', 11 => 'eleven', 12 => 'tweleve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen',
+                  17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen', 20 => 'twenty', 30 => 'thirty', 40 => 'forty', 50 => 'fifty',
+                  60 => 'sixty', 70 => 'seventy', 80 => 'eighty', 90 => 'ninety'}
+
+  def digit_1(num)
+    return NUMBER_WORD[num]
+  end
+
 end
--- number_word_spec.rb
+++ number_word_spec.rb
@@ -1,2 +1,20 @@
 require 'rspec'
 require './number_word'
+
+describe NumberWord do
+
+  before {
+    @number_word = NumberWord.new
+  }
+
+  describe '#digit_1' do
+    it 'should return one' do
+      expect(@number_word.digit_1(1)).to eq('one')
+    end
+
+    it 'should return eight' do
+      expect(@number_word.digit_1(8)).to eq('eight')
+    end
+  end
+
+end
