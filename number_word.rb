a0897f0bc376e2921e7921ca9f08d19ac9ba8e0f
Kumar <akarsh.mit@gmail.com>
2015-06-09 20:39:58 +0530

    Fixing indentation in constant number_word hash

--- number_word.rb
+++ number_word.rb
@@ -2,7 +2,8 @@ class NumberWord
 
   NUMBER_WORD = { 0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight',
                   9 => 'nine', 10 => 'ten', 11 => 'eleven', 12 => 'tweleve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen',
-                  16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen', 20 => 'twenty', 30 => 'thirty', 40 => 'forty',                       50 => 'fifty', 60 => 'sixty', 70 => 'seventy', 80 => 'eighty', 90 => 'ninety'}
+                  16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen', 20 => 'twenty', 30 => 'thirty', 40 => 'forty',
+                  50 => 'fifty', 60 => 'sixty', 70 => 'seventy', 80 => 'eighty', 90 => 'ninety'}
 
   def digits_count(num)
     num.to_s.size
